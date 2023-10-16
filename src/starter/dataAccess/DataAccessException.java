ad81a8a353b2ba80f1be630b70f1ef2e1534c60d
Sam Jones <sam.jones4844@gmail.com>
2023-10-16 16:00:22 -0600

    Added data access package layout

---
+++ src/starter/dataAccess/DataAccessException.java
@@ -0,0 +1,12 @@
+package dataAccess;
+
+/**
+ * Indicates there was an error connecting to the database
+ */
+public class DataAccessException extends Exception
+{
+    public DataAccessException(String message)
+    {
+        super(message);
+    }
+}
