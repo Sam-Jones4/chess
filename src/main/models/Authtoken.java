674ed89c61a38aef86387c7868aa7968f6d04b0e
Sam Jones <sam.jones4844@gmail.com>
2023-10-26 13:31:58 -0600

    Created Constructors, getters, and setters for the model classes I forgot earlier

--- src/main/models/Authtoken.java
+++ src/main/models/Authtoken.java
@@ -13,4 +13,36 @@ public class Authtoken
      * creates a variable to store a username
      */
     private String username;
+
+    /**
+     * Class constructor
+     *
+     * @param authToken sets authToken to the authToken
+     * @param username sets username to the username
+     */
+    public Authtoken(String authToken, String username)
+    {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getAuthToken()
+    {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken)
+    {
+        this.authToken = authToken;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
 }
--- src/main/models/Game.java
+++ src/main/models/Game.java
@@ -29,4 +29,72 @@ public class Game
      * Creates an object to store a Game
      */
     private ChessGameImpl game;
+
+    /**
+     * Class constructor
+     *
+     * @param gameID sets gameID to the game ID
+     * @param whiteUsername sets whiteUsername to white's username
+     * @param blackUsername sets blackUsername to black's username
+     * @param gameName sets gameName to the game name
+     * @param game sets game to teh game
+     */
+    public Game(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGameImpl game)
+    {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+    public int getGameID()
+    {
+        return gameID;
+    }
+
+    public void setGameID(int gameID)
+    {
+        this.gameID = gameID;
+    }
+
+    public String getWhiteUsername()
+    {
+        return whiteUsername;
+    }
+
+    public void setWhiteUsername(String whiteUsername)
+    {
+        this.whiteUsername = whiteUsername;
+    }
+
+    public String getBlackUsername()
+    {
+        return blackUsername;
+    }
+
+    public void setBlackUsername(String blackUsername)
+    {
+        this.blackUsername = blackUsername;
+    }
+
+    public String getGameName()
+    {
+        return gameName;
+    }
+
+    public void setGameName(String gameName)
+    {
+        this.gameName = gameName;
+    }
+
+    public ChessGameImpl getGame()
+    {
+        return game;
+    }
+
+    public void setGame(ChessGameImpl game)
+    {
+        this.game = game;
+    }
 }
