9fd60de259c24774cd49f832820f8e7a7f9799e2
Sam Jones <sam.jones4844@gmail.com>
2023-10-25 20:39:12 -0600

    Created RegisterHandler

--- src/main/handlers/RegisterHandler.java
+++ src/main/handlers/RegisterHandler.java
@@ -1,5 +1,12 @@
 package handlers;
 
+import com.google.gson.Gson;
+import requests.JoinGameRequest;
+import requests.RegisterRequest;
+import responses.JoinGameResponse;
+import responses.RegisterResponse;
+import services.JoinGameService;
+import services.RegisterService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -10,6 +17,28 @@ public class RegisterHandler implements Route
     @Override
     public Object handle(Request request, Response response) throws Exception
     {
-        return null;
+        RegisterRequest registerRequest = new Gson().fromJson(request.body(), RegisterRequest.class);
+
+        RegisterService service = new RegisterService();
+        RegisterResponse result = service.registerUser(registerRequest);
+
+        if (result.getUsername() != null && result.getAuthToken() != null)
+        {
+            response.status(200);
+        }
+        else if (result.getMessage() == "Error: bad request")
+        {
+            response.status(400);
+        }
+        else if (result.getMessage() == "Error: already taken")
+        {
+            response.status(403);
+        }
+        else
+        {
+            response.status(500);
+        }
+
+        return new Gson().toJson(result);
     }
 }
