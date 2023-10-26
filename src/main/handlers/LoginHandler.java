e58600c44eb6cc704249a61a5a8d7d6a317275e2
Sam Jones <sam.jones4844@gmail.com>
2023-10-25 20:32:34 -0600

    Created LoginHandler

--- src/main/handlers/LoginHandler.java
+++ src/main/handlers/LoginHandler.java
@@ -1,5 +1,12 @@
 package handlers;
 
+import com.google.gson.Gson;
+import requests.JoinGameRequest;
+import requests.LoginRequest;
+import responses.JoinGameResponse;
+import responses.LoginResponse;
+import services.JoinGameService;
+import services.LoginService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -10,6 +17,24 @@ public class LoginHandler implements Route
     @Override
     public Object handle(Request request, Response response) throws Exception
     {
-        return null;
+        LoginRequest loginRequest = new Gson().fromJson(request.body(), LoginRequest.class);
+
+        LoginService service = new LoginService();
+        LoginResponse result = service.login(loginRequest);
+
+        if (result.getMessage() == null)
+        {
+            response.status(200);
+        }
+        else if(result.getMessage() == "Error: unauthorized")
+        {
+            response.status(401);
+        }
+        else
+        {
+            response.status(500);
+        }
+
+        return new Gson().toJson(result);
     }
 }
