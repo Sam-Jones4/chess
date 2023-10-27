fb4b1a9a1090e247e3f8fe0edb952509b9fe8ad2
Sam Jones <sam.jones4844@gmail.com>
2023-10-27 14:26:25 -0600

    Created ClearApplicationHandler and started its service class

--- src/main/handlers/ClearApplicationHandler.java
+++ src/main/handlers/ClearApplicationHandler.java
@@ -1,5 +1,7 @@
 package handlers;
 
+import responses.ClearApplicationResponse;
+import services.ClearApplicationService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -9,6 +11,20 @@ public class ClearApplicationHandler implements Route
     @Override
     public Object handle(Request request, Response response) throws Exception
     {
+        String token = request.headers("/db");
+
+        ClearApplicationService service = new ClearApplicationService();
+        ClearApplicationResponse result = service.clearApplication(token);
+
+        if (result.getMessage() == null)
+        {
+            response.status(200);
+        }
+        else
+        {
+            response.status(500);
+        }
+
         return null;
     }
 }
--- src/main/services/ClearApplicationService.java
+++ src/main/services/ClearApplicationService.java
@@ -1,11 +1,18 @@
 package services;
 
+import dataAccess.AuthDAO;
 import responses.ClearApplicationResponse;
 
 public class ClearApplicationService
 {
-    public ClearApplicationResponse clearApplication()
+    public ClearApplicationResponse clearApplication(String token)
     {
+        //check is the authtoken is valid, if invalid send 401
+        //check the request object is valid,
+        AuthDAO authDAO = new AuthDAO();
+        ClearApplicationService
+        authDAO.getAuthtoken()
+
         return null;
     }
 
