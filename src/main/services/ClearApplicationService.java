8fb490ffa48fbab93ae28ddb82f00c6525802eb1
Sam Jones <sam.jones4844@gmail.com>
2023-10-31 11:23:20 -0600

    Created clear app service

--- src/main/services/ClearApplicationService.java
+++ src/main/services/ClearApplicationService.java
@@ -1,19 +1,24 @@
 package services;
 
 import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.UserDAO;
+import models.Game;
 import responses.ClearApplicationResponse;
 
 public class ClearApplicationService
 {
     public ClearApplicationResponse clearApplication(String token)
     {
-        //check is the authtoken is valid, if invalid send 401
-        //check the request object is valid,
         AuthDAO authDAO = new AuthDAO();
-        ClearApplicationService
-        authDAO.getAuthtoken()
+        UserDAO userDAO = new UserDAO();
+        GameDAO gameDAO = new GameDAO();
 
-        return null;
+        authDAO.clearAuthtokens();
+        userDAO.clearUsers();
+        gameDAO.clearGames();
+
+        return new ClearApplicationResponse(null);
     }
 
 }
