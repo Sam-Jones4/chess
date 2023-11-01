0c975dd19f32d3604505c4c5b3d216c089238545
Sam Jones <sam.jones4844@gmail.com>
2023-11-01 15:09:21 -0600

    Finished join game service

--- src/main/services/JoinGameService.java
+++ src/main/services/JoinGameService.java
@@ -1,5 +1,10 @@
 package services;
 
+import chess.ChessGame;
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.UserDAO;
+import models.Authtoken;
 import requests.JoinGameRequest;
 import responses.JoinGameResponse;
 
@@ -7,7 +12,41 @@ public class JoinGameService
 {
     public JoinGameResponse joinGame(JoinGameRequest joinGameRequest, String token)
     {
-        return null;
+        GameDAO gameDAO = new GameDAO();
+        AuthDAO authDAO = new AuthDAO();
+
+        if (joinGameRequest.getGameID() == 0 || joinGameRequest.getPlayerColor() == null)
+        {
+            return new JoinGameResponse("Error: bad request");
+        }
+        if (authDAO.findAuthtoken(token) == null)
+        {
+            return new JoinGameResponse("Error: unauthorized");
+        }
+
+        if (gameDAO.findGame(joinGameRequest.getGameID()) != null)
+        {
+            if (joinGameRequest.getPlayerColor() == ChessGame.TeamColor.BLACK)
+            {
+                if (gameDAO.findGame(joinGameRequest.getGameID()).getBlackUsername() != null)
+                {
+                    return new JoinGameResponse("Error: already taken");
+                }
+            }
+            if (joinGameRequest.getPlayerColor() == ChessGame.TeamColor.WHITE)
+            {
+                if (gameDAO.findGame(joinGameRequest.getGameID()).getWhiteUsername() != null)
+                {
+                    return new JoinGameResponse("Error: already taken");
+                }
+            }
+        }
+
+        Authtoken authtoken = authDAO.findAuthtoken(token);
+
+        gameDAO.claimSpot(joinGameRequest.getGameID(), authtoken.getUsername(), joinGameRequest.getPlayerColor());
+
+        return new JoinGameResponse();
     }
 
 }
