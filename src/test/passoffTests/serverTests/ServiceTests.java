66b806b79505fce6f4dbc17fd19f803c4b7cebbd
Sam Jones <sam.jones4844@gmail.com>
2023-11-01 21:19:32 -0600

    Finished unit tests

---
+++ src/test/passoffTests/serverTests/ServiceTests.java
@@ -0,0 +1,246 @@
+package passoffTests.serverTests;
+
+import chess.ChessGame;
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.UserDAO;
+import models.Authtoken;
+import models.Game;
+import models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requests.CreateGameRequest;
+import requests.JoinGameRequest;
+import requests.LoginRequest;
+import requests.RegisterRequest;
+import responses.*;
+import services.*;
+
+import java.util.ArrayList;
+
+public class ServiceTests
+{
+    @BeforeEach
+    void ClearTestData()
+    {
+        ClearApplicationService clearApplicationService = new ClearApplicationService();
+        clearApplicationService.clearApplication();
+    }
+
+
+    @Test
+    void RegisterPassTest()
+    {
+        RegisterService registerService = new RegisterService();
+        RegisterRequest registerRequest = new RegisterRequest("samuser", "password", "sam@gmail.com");
+
+        RegisterResponse response = registerService.registerUser(registerRequest);
+
+        Assertions.assertEquals(registerRequest.getUsername(), response.getUsername());
+        Assertions.assertNotNull(response.getAuthToken());
+        Assertions.assertNull(response.getMessage());
+    }
+
+    @Test
+    void RegisterFailTest()
+    {
+        RegisterService registerService = new RegisterService();
+        RegisterRequest registerRequest = new RegisterRequest("samuser", null, "sam@gmail.com");
+
+        RegisterResponse response = registerService.registerUser(registerRequest);
+
+        Assertions.assertEquals(response.getMessage(), "Error: bad request");
+    }
+
+    @Test
+    void LogoutPassTest()
+    {
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        LogoutService logoutService = new LogoutService();
+        String authToken = "thisismytoken";
+
+        LogoutResponse response = logoutService.logout(authToken);
+
+        Assertions.assertNull(authDAO.findAuthtoken("thisismytoken"));
+        Assertions.assertNull(response.getMessage());
+    }
+
+    @Test
+    void LogoutFailTest()
+    {
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        LogoutService logoutService = new LogoutService();
+        String authToken = "thisisNOTmytoken";
+
+        LogoutResponse response = logoutService.logout(authToken);
+
+        Assertions.assertNotNull(authDAO.findAuthtoken("thisismytoken"));
+        Assertions.assertEquals(response.getMessage(), "Error: unauthorized");
+    }
+
+    @Test
+    void LoginPassTest()
+    {
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samsusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        UserDAO userDAO = new UserDAO();
+        userDAO.insertUser("samsusername", "samspassword", "email");
+
+        LoginService loginService = new LoginService();
+        LoginRequest loginRequest = new LoginRequest("samsusername", "samspassword");
+
+        LoginResponse response = loginService.login(loginRequest);
+
+        Assertions.assertEquals(loginRequest.getUsername(), authtoken.getUsername());
+        Assertions.assertNotNull(authDAO.findAuthtoken("thisismytoken"));
+        Assertions.assertNull(response.getMessage());
+    }
+
+    @Test
+    void LoginFailTest()
+    {
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisisNOTmytoken", "samsusername");
+
+        LoginService loginService = new LoginService();
+        LoginRequest loginRequest = new LoginRequest("samsusername", "samspassword");
+
+        LoginResponse response = loginService.login(loginRequest);
+
+        Assertions.assertEquals(loginRequest.getUsername(), authtoken.getUsername());
+        Assertions.assertEquals(response.getAuthToken(), authDAO.findAuthtoken("thisismytoken"));
+        Assertions.assertEquals(response.getMessage(), "Error: unauthorized");
+    }
+
+    @Test
+    void ListGamesPassTest()
+    {
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samsusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        GameDAO gameDAO = new GameDAO();
+        Game game1 = new Game(1,"game1");
+        Game game2 = new Game(2,"game2");
+        gameDAO.insertGame(game1);
+        gameDAO.insertGame(game2);
+        ArrayList<Game> gameArray = new ArrayList<>();
+
+        gameArray.add(game1);
+        gameArray.add(game2);
+
+        ListGamesService listGamesService = new ListGamesService();
+
+        ListGamesResponse listGamesResponse = listGamesService.listGames("thisismytoken");
+
+        Assertions.assertNotNull(authDAO.findAuthtoken("thisismytoken"));
+        Assertions.assertEquals(listGamesResponse.getGames(), gameArray);
+    }
+
+    @Test
+    void ListGamesFailTest()
+    {
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        GameDAO gameDAO = new GameDAO();
+        Game game1 = new Game(1,"game1");
+        Game game2 = new Game(2,"game2");
+        gameDAO.insertGame(game1);
+        gameDAO.insertGame(game2);
+
+        ListGamesService listGamesService = new ListGamesService();
+
+        ListGamesResponse listGamesResponse = listGamesService.listGames("thisisNOTmytoken");
+
+        Assertions.assertNull(authDAO.findAuthtoken("thisisNOTmytoken"));
+        Assertions.assertEquals(listGamesResponse.getMessage(), "Error: unauthorized");
+    }
+
+    @Test
+    void JoinGamePassTest()
+    {
+        GameDAO gameDAO = new GameDAO();
+        Game game1 = new Game(1,"game1");
+        gameDAO.insertGame(game1);
+
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        JoinGameService joinGameService = new JoinGameService();
+        JoinGameRequest joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.WHITE, 1);
+
+        JoinGameResponse response = joinGameService.joinGame(joinGameRequest, "thisismytoken");
+
+        Assertions.assertEquals("samusername", gameDAO.findGame(1).getWhiteUsername());
+        Assertions.assertNull(response.getMessage());
+    }
+
+    @Test
+    void JoinGameFailTest()
+    {
+        JoinGameService joinGameService = new JoinGameService();
+        JoinGameRequest joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.WHITE, 0);
+
+        JoinGameResponse response = joinGameService.joinGame(joinGameRequest, "thisismytoken");
+
+        Assertions.assertEquals(response.getMessage(), "Error: bad request");
+    }
+
+    @Test
+    void CreateGamePassTest()
+    {
+        GameDAO gameDAO = new GameDAO();
+
+        AuthDAO authDAO = new AuthDAO();
+        Authtoken authtoken = new Authtoken("thisismytoken", "samusername");
+        authDAO.insertAuthtoken(authtoken);
+
+        CreateGameService createGameService = new CreateGameService();
+        CreateGameRequest createGameRequest = new CreateGameRequest("myGame");
+
+        CreateGameResponse response = createGameService.createGame(createGameRequest, "thisismytoken");
+
+        Assertions.assertNotNull(response.getGameID());
+        Assertions.assertEquals("myGame", gameDAO.findGame(response.getGameID()).getGameName());
+
+    }
+
+    @Test
+    void CreateGameFailTest()
+    {
+        CreateGameService createGameService = new CreateGameService();
+        CreateGameRequest createGameRequest = new CreateGameRequest(null);
+
+        CreateGameResponse response = createGameService.createGame(createGameRequest, "thisismytoken");
+
+        Assertions.assertEquals(response.getMessage(), "Error: bad request");
+    }
+
+    @Test
+    void ClearApplicationTest()
+    {
+        UserDAO userDAO = new UserDAO();
+        userDAO.insertUser("samuser", "password", "sam@gmail.com");
+
+        ClearApplicationService clearApplicationService = new ClearApplicationService();
+
+        ClearApplicationResponse response = clearApplicationService.clearApplication();
+
+        Assertions.assertNull(userDAO.findUser("samuser"));
+
+    }
+
+
+}
