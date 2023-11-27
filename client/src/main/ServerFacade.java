45fa11ea772b10c37cd4e634714a35d5fd320562
Sam Jones <sam.jones4844@gmail.com>
2023-11-27 12:38:23 -0700

    Created ServerFacade and began programming it

---
+++ client/src/main/ServerFacade.java
@@ -0,0 +1,65 @@
+import com.google.gson.Gson;
+import requests.RegisterRequest;
+import responses.RegisterResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+public class ServerFacade
+{
+    String baseURL = "http://localhost:8080/";
+
+    public RegisterResponse register(RegisterRequest request) throws IOException
+    {
+        HttpURLConnection http = sendRequest(baseURL + "user", "POST", new Gson().toJson(request));
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            return new Gson().fromJson(inputStreamReader, RegisterResponse.class);
+        } catch (Exception exception)
+        {
+            return null;
+        }
+    }
+
+
+    private static HttpURLConnection sendRequest(String url, String method, String body) throws URISyntaxException, IOException {
+        URI uri = new URI(url);
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod(method);
+        writeRequestBody(body, http);
+        http.connect();
+        System.out.printf("= Request =========\n[%s] %s\n\n%s\n\n", method, url, body);
+        return http;
+    }
+
+    private static void writeRequestBody(String body, HttpURLConnection http) throws IOException {
+        if (!body.isEmpty()) {
+            http.setDoOutput(true);
+            try (var outputStream = http.getOutputStream()) {
+                outputStream.write(body.getBytes());
+            }
+        }
+    }
+
+    private static Object receiveResponse(HttpURLConnection http) throws IOException {
+        var statusCode = http.getResponseCode();
+        var statusMessage = http.getResponseMessage();
+
+        Object responseBody = readResponseBody(http);
+
+        return responseBody;
+    }
+
+    private static Object readResponseBody(HttpURLConnection http) throws IOException {
+        Object responseBody = "";
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            responseBody = new Gson().fromJson(inputStreamReader, RegisterResponse.class);
+        }
+        return responseBody;
+    }
+}
+
