189ef1b828be7c71317c2349d93f1e3fb868fb18
Sam Jones <sam.jones4844@gmail.com>
2023-11-29 14:54:36 -0700

    Began programming EnterCommand for PostloginUI and altered other files accordingly

--- client/src/main/ui/PostloginUI.java
+++ client/src/main/ui/PostloginUI.java
@@ -1,10 +1,72 @@
 package ui;
 
+import facade.ServerFacade;
+import requests.CreateGameRequest;
+import responses.CreateGameResponse;
+
+import java.util.Scanner;
+
 public class PostloginUI
 {
     public static void EnterCommand()
     {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true)
+        {
+            String command = scanner.next();
+            if (command == "create")
+            {
+                System.out.println("Game name:");
+                String gameName = scanner.next();
+
+                CreateGameRequest request = new CreateGameRequest(gameName);
+
+                try
+                {
+                    CreateGameResponse result = ServerFacade.createGame(request);
+
+                    assert result != null;
+                    if (result.getMessage() != null)
+                    {
+                        System.out.println(result.getMessage());
+                    }
+                    else
+                    {
+                        PostloginUI.EnterCommand();
+                    }
+
+                } catch (Exception exception)
+                {
+                    System.out.println(exception.getMessage());
+                }
+
+            }
+            else if (command == "list")
+            {
+
+            }
+            else if (command == "join")
+            {
+
+            }
+            else if (command == "observe")
+            {
+
+            }
+            else if (command == "logout")
+            {
+
+            }
+            else if (command == "quit")
+            {
+
+            }
+            else if (command == "help")
+            {
+
+            }
+        }
     }
 
 }
