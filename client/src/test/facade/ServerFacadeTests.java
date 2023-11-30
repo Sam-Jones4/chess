5ae601f2a12c96c79e951762c54a8d3e9653ace7
Sam Jones <sam.jones4844@gmail.com>
2023-11-29 17:47:43 -0700

    Added shell for unit tests

--- client/src/test/facade/ServerFacadeTests.java
+++ client/src/test/facade/ServerFacadeTests.java
@@ -1,6 +1,84 @@
 package facade;
 
+import org.junit.jupiter.api.Test;
+
 public class ServerFacadeTests
 {
+    @Test
+    void ClearApplicationTest()
+    {
 
+    }
+
+    @Test
+    void RegisterPassTest()
+    {
+
+    }
+
+    @Test
+    void RegisterFailTest()
+    {
+
+    }
+
+    @Test
+    void LoginPassTest()
+    {
+
+    }
+
+    @Test
+    void LoginFailTest()
+    {
+
+    }
+
+    @Test
+    void LogoutPassTest()
+    {
+
+    }
+
+    @Test
+    void LogoutFailTest()
+    {
+
+    }
+
+    @Test
+    void CreateGamePassTest()
+    {
+
+    }
+
+    @Test
+    void CreateGameFailTest()
+    {
+
+    }
+
+    @Test
+    void JoinGamePassTest()
+    {
+
+    }
+
+    @Test
+    void JoinGameFailTest()
+    {
+
+    }
+
+    @Test
+    void ListGamesPassTest()
+    {
+
+    }
+
+    @Test
+    void ListGamesFailTest()
+    {
+
+    }
 }
