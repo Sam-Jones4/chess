c60da750cc35cbfc634eef0f72b73d3627ada3d3
Sam Jones <sam.jones4844@gmail.com>
2023-11-29 18:38:34 -0700

    Added white board

--- client/src/main/ui/GameUI.java
+++ client/src/main/ui/GameUI.java
@@ -4,25 +4,26 @@ import chess.*;
 
 public class GameUI
 {
-    public static void PrintWhiteBoard(ChessGameImpl game)
+    public static void PrintBlackBoard(ChessGameImpl game)
     {
         ChessBoard board = game.getBoard();
 
-        String squareColor = EscapeSequences.SET_BG_COLOR_BLACK;
+        String squareColor = EscapeSequences.SET_BG_COLOR_GREEN;
 
         for (int i = 1; i < 9; i++)
         {
+            System.out.print(squareColor);
             for (int j = 1; j < 9; j++)
             {
                 System.out.print(squareColor);
 
-                if (squareColor.equals(EscapeSequences.SET_BG_COLOR_BLACK))
+                if (squareColor.equals(EscapeSequences.SET_BG_COLOR_GREEN))
                 {
                     squareColor = EscapeSequences.SET_BG_COLOR_WHITE;
                 }
                 else
                 {
-                    squareColor = EscapeSequences.SET_BG_COLOR_BLACK;
+                    squareColor = EscapeSequences.SET_BG_COLOR_GREEN;
                 }
 
                 ChessPositionImpl position = new ChessPositionImpl(i,j);
@@ -105,9 +106,106 @@ public class GameUI
         }
     }
 
-    public static void PrintBlackBoard(ChessGameImpl game)
+    public static void PrintWhiteBoard(ChessGameImpl game)
     {
+        ChessBoard board = game.getBoard();
 
+        String squareColor = EscapeSequences.SET_BG_COLOR_GREEN;
+
+        for (int i = 1; i < 9; i++)
+        {
+            System.out.print(squareColor);
+            for (int j = 1; j < 9; j++)
+            {
+                System.out.print(squareColor);
+
+                if (squareColor.equals(EscapeSequences.SET_BG_COLOR_GREEN))
+                {
+                    squareColor = EscapeSequences.SET_BG_COLOR_WHITE;
+                }
+                else
+                {
+                    squareColor = EscapeSequences.SET_BG_COLOR_GREEN;
+                }
+
+                ChessPositionImpl position = new ChessPositionImpl(i,j);
+                if (board.getPiece(position) != null)
+                {
+                    if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.KING)
+                    {
+                        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
+                        {
+                            System.out.print(EscapeSequences.WHITE_KING);
+                        }
+                        else
+                        {
+                            System.out.print(EscapeSequences.BLACK_KING);
+                        }
+                    }
+                    else if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.QUEEN)
+                    {
+                        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
+                        {
+                            System.out.print(EscapeSequences.WHITE_QUEEN);
+                        }
+                        else
+                        {
+                            System.out.print(EscapeSequences.BLACK_QUEEN);
+                        }
+                    }
+                    else if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.ROOK)
+                    {
+                        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
+                        {
+                            System.out.print(EscapeSequences.WHITE_ROOK);
+                        }
+                        else
+                        {
+                            System.out.print(EscapeSequences.BLACK_ROOK);
+                        }
+                    }
+                    else if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.BISHOP)
+                    {
+                        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
+                        {
+                            System.out.print(EscapeSequences.WHITE_BISHOP);
+                        }
+                        else
+                        {
+                            System.out.print(EscapeSequences.BLACK_BISHOP);
+                        }
+                    }
+                    else if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.KNIGHT)
+                    {
+                        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
+                        {
+                            System.out.print(EscapeSequences.WHITE_KNIGHT);
+                        }
+                        else
+                        {
+                            System.out.print(EscapeSequences.BLACK_KNIGHT);
+                        }
+                    }
+                    else if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.PAWN)
+                    {
+                        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
+                        {
+                            System.out.print(EscapeSequences.WHITE_PAWN);
+                        }
+                        else
+                        {
+                            System.out.print(EscapeSequences.BLACK_PAWN);
+                        }
+                    }
+
+                }
+                else
+                {
+                    System.out.print(EscapeSequences.EMPTY);
+                }
+            }
+            System.out.println();
+        }
     }
 
 
